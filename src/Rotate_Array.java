a49777cba8c41f54e08178e31f9b607ec085a429
Defu95 <wangdefu95@gmail.com>
2018-09-03 21:49:05 +0800

    leetcode 189

---
+++ src/Rotate_Array.java
@@ -0,0 +1,19 @@
+public class Rotate_Array {
+
+    public static void main(String[] args){
+        int[] nums={1,2,3,4,5,6,7};
+        int k = 3;
+        rotate1(nums,k);
+        int i=0;
+        while (i<nums.length){
+            System.out.print(nums[i]+" ");
+            i++;
+        }
+    }
+
+    public static void rotate1(int[] nums, int k) {
+        if(k==0)
+            return;
+        int[] tmp=
+    }
+}
--- src/testgit.java
+++
@@ -1,5 +0,0 @@
-public class testgit {
-    public static void main(String[] args){
-        System.out.print("hello world!!!");
-    }
-}
