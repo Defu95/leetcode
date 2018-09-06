af4b0931cd7ff6832c0a78b769ce19e51c1d7d3c
Defu95 <wangdefu95@gmail.com>
2018-09-07 00:05:22 +0800

    leetcode 443

--- src/String_Compression.java
+++ src/String_Compression.java
@@ -5,8 +5,12 @@ import java.util.Iterator;
 
 public class String_Compression {
     public static void main(String[] args) {
-        char[] chars={'a','a','b','b','c','c','c'};
+        char[] chars={'a','a','a','b','b','a','a','d'};
+//        char[] chars={'a','a','b','b','c','c','c'};
+//        char[] chars={'a'};
         System.out.print(compress(chars));
+        for(int i=0;i<chars.length;i++)
+            System.out.print(chars[i]);
     }
 
     public static int compress(char[] chars) {
@@ -28,12 +32,30 @@ public class String_Compression {
 //                count+=String.valueOf(tmp).length();
 //        }
 //        return count;
-        Arrays.sort(chars);
-        int count=0;
+//        Arrays.sort(chars);
+        int count=1,flag=0;
         for(int i=0;i<chars.length-1;i++){
-            if(chars[i]!=chars[i+1])
+            if(chars[i]==chars[i+1])
                 count++;
+            else{
+                chars[flag]=chars[i];
+                flag++;
+                if(count>1){
+                    String s=Integer.toString(count);
+                    for(int m=0;m<s.length();m++)
+                        chars[flag+m]=s.charAt(m);
+                    flag+=s.length();
+                }
+                count=1;
+            }
         }
-
+        chars[flag++]=chars[chars.length-1];
+        String co=Integer.toString(count);
+        if(count>1){
+            for(int m=0;m<co.length();m++)
+                chars[flag+m]=co.charAt(m);
+            flag+=co.length();
+        }
+        return flag;
     }
 }
