4d4c5f823f9a2bba8a9e3bc03c8fd28009a6bf16
Defu95 <wangdefu95@gmail.com>
2018-09-07 13:06:32 +0800

    leetcode temp

--- src/K_diff_Pairs_in_an_Array.java
+++ src/K_diff_Pairs_in_an_Array.java
@@ -2,9 +2,9 @@ import java.util.HashSet;
 
 public class K_diff_Pairs_in_an_Array {
     public static void main(String[] args){
-//        int[] nums={3, 1, 4, 1, 5};int  k = 2;
+        int[] nums={3, 1, 4, 1, 5};int  k = 2;
 //        int[] nums={1, 3, 1, 5, 4}; int k = 0;
-        int[] nums={1, 2, 3, 4, 5}; int k = 3;
+//        int[] nums={1, 2, 3, 4, 5}; int k = 3;
         System.out.print(findPairs(nums,k));
     }
 
@@ -12,15 +12,23 @@ public class K_diff_Pairs_in_an_Array {
         HashSet<Integer> set=new HashSet<>();
         int count=0;
         for(int i=0;i<nums.length;i++){
-            if(!set.contains(nums[i])){
-                if(set.contains(k-nums[i])||set.contains(nums[i]-k))
-                    count++;
-                set.add(nums[i]);
-            }else{
-                if(set.contains(k-nums[i])||set.contains(nums[i]-k))
-                    count++;
+            if(set.contains(k+nums[i])||set.contains(nums[i]-k)){
+                count++;
+                if(!set.contains(nums[i]))
+                    set.add(nums[i]);
+            }  else{
+                if(!set.contains(nums[i]))
+                    set.add(nums[i]);
             }
         }
         return count;
+//        for(int i=0;i<nums.length-1;i++){
+////            if(i+1<nums.length){
+//                for(int j=i+1;j<nums.length;j++)
+//                    if((nums[i]-nums[j]==k)||(nums[j]-nums[i]==k))
+//                        count++;
+////            }
+//        }
+//        return count;
     }
 }
