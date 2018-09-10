5b66ca1239bdd502b7be0d6815c0a25f90a11388
Defu95 <wangdefu95@gmail.com>
2018-09-11 00:19:41 +0800

    leetcode 605

--- src/Can_Place_Flowers.java
+++ src/Can_Place_Flowers.java
@@ -1,8 +1,11 @@
-import java.util.HashMap;
-
 public class Can_Place_Flowers {
     public static void main(String[] args){
-
+//        int[] flowerbed = {1,0,0,0,1};
+//        int[] flowerbed={0,0,1,0,1};
+//        int[] flowerbed={0,1,0,1,0,0};
+        int[] flowerbed={0,0};
+        int n = 2;
+        System.out.println(canPlaceFlowers(flowerbed,n));
     }
     public static boolean canPlaceFlowers(int[] flowerbed, int n) {
         if(n==0)
@@ -17,17 +20,21 @@ public class Can_Place_Flowers {
         if(flag==-1&&n>0)
             return false;
         for(int i=flag;i<flowerbed.length;i++){
-            int lock=-1,count=1;
-            if(flowerbed[i]==0){
-                if(lock==1)
+            int count=1;
+            int j=i+1;
+            for(;j<flowerbed.length;j++){
+                if(flowerbed[j]==0)
                     count++;
-                else{
-                    lock=1;
-                    flag=i;
-                }
+                else
+                    break;
             }
-
+            if(i<flowerbed.length&&j==flowerbed.length)
+                count++;
+            if(i==0)
+                count++;
+            i=j;
+            n-=(count-1)/2;
         }
-
+        return n<=0? true : false;
     }
 }
