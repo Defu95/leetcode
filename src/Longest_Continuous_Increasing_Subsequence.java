cd03b504c5be8436725e456e05d88d97f65a07da
Defu95 <wangdefu95@gmail.com>
2018-09-12 22:48:15 +0800

    leetcode 674

--- src/Longest_Continuous_Increasing_Subsequence.java
+++ src/Longest_Continuous_Increasing_Subsequence.java
@@ -1,7 +1,7 @@
 public class Longest_Continuous_Increasing_Subsequence {
     public static void main(String[] args){
-//        int[] nums={1,3,5,7,1,2,3,4,5,6};
-        int[] nums={1,2,3,4,2,5};
+        int[] nums={1,3,5,7,1,2,3,4,5,6};
+//        int[] nums={1,2,3,4,2,5};
 //        int[] nums={1,1,1};
         System.out.print(findLengthOfLCIS(nums));
     }
@@ -9,18 +9,25 @@ public class Longest_Continuous_Increasing_Subsequence {
         int l=nums.length;
         if(l<1)
             return 0;
-        int maxlen=1;
-        for(int i=0;i<l;i++){
-            for(int j=i+1;j<l;j++) {
-                if (nums[j] <= nums[j - 1]) {
-                    maxlen = Math.max(maxlen, j - i);
-                    i = j-1 ;
-                    break;
-                }
-                if(j==l-1)
-                    maxlen=Math.max(maxlen,j-i+1);
+//        int maxlen=1;
+//        for(int i=0;i<l;i++){
+//            for(int j=i+1;j<l;j++) {
+//                if (nums[j] <= nums[j - 1]) {
+//                    maxlen = Math.max(maxlen, j - i);
+//                    i = j-1 ;
+//                    break;
+//                }
+//                if(j==l-1)
+//                    maxlen=Math.max(maxlen,j-i+1);
+//            }
+//        }
+        int maxlen=0,start=0;
+        for(int i=1;i<l;i++){
+            if(nums[i]<=nums[i-1]){
+                maxlen=Math.max(maxlen,i-start);
+                start=i;
             }
         }
-        return maxlen;
+        return Math.max(maxlen,l-start);
     }
 }
