0877d7035b4f41388239a2c9a95a79494a151cb1
Defu95 <wangdefu95@gmail.com>
2018-09-13 00:07:48 +0800

    leetcode 746

--- src/Min_Cost_Climbing_Stairs.java
+++ src/Min_Cost_Climbing_Stairs.java
@@ -1,2 +1,15 @@
 public class Min_Cost_Climbing_Stairs {
+    public static void main(String[] args){
+         int[] cost={1, 100, 1, 1, 1, 100, 1, 1, 100, 1};
+         System.out.println(minCostClimbingStairs(cost));
+    }
+    public static int minCostClimbingStairs(int[] cost) {
+        int length=cost.length;
+        int[] dp=new int[length+1];
+        dp[0]=0;dp[1]=0;
+        for(int i=2;i<=length;i++){
+            dp[i]=Math.min(dp[i-2]+cost[i-2],dp[i-1]+cost[i-1]);
+        }
+        return dp[length];
+    }
 }
