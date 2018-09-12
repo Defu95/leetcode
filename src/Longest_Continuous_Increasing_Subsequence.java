bd04fb572cee56840ab3d2a8999e4cf390a41313
Defu95 <wangdefu95@gmail.com>
2018-09-12 22:44:42 +0800

    leetcode 674

---
+++ src/Longest_Continuous_Increasing_Subsequence.java
@@ -0,0 +1,26 @@
+public class Longest_Continuous_Increasing_Subsequence {
+    public static void main(String[] args){
+//        int[] nums={1,3,5,7,1,2,3,4,5,6};
+        int[] nums={1,2,3,4,2,5};
+//        int[] nums={1,1,1};
+        System.out.print(findLengthOfLCIS(nums));
+    }
+    public static int findLengthOfLCIS(int[] nums) {
+        int l=nums.length;
+        if(l<1)
+            return 0;
+        int maxlen=1;
+        for(int i=0;i<l;i++){
+            for(int j=i+1;j<l;j++) {
+                if (nums[j] <= nums[j - 1]) {
+                    maxlen = Math.max(maxlen, j - i);
+                    i = j-1 ;
+                    break;
+                }
+                if(j==l-1)
+                    maxlen=Math.max(maxlen,j-i+1);
+            }
+        }
+        return maxlen;
+    }
+}
