af555a322dd970146a025b6138d68dc93612f7bb
Defu95 <wangdefu95@gmail.com>
2018-09-13 10:16:17 +0800

    leetcode 448

--- src/Find_All_Numbers_Disappeared_in_an_Array.java
+++ src/Find_All_Numbers_Disappeared_in_an_Array.java
@@ -10,46 +10,31 @@ public class Find_All_Numbers_Disappeared_in_an_Array {
     }
     public static List<Integer> findDisappearedNumbers(int[] nums) {
         List<Integer> res=new ArrayList<>();
-//        if(nums.length<=0)
-//            return res;
-//        Arrays.sort(nums);
-//        if(nums[0]!=1){
-//            int j=1;
-//            while (j<nums[0]){
-//                res.add(j);
-//                j++;
-//            }
-//        }
-//        for(int i=1;i<nums.length;i++)
-//            if(nums[i]!=nums[i-1]&&nums[i]!=nums[i-1]+1) {
-//                int j = 1;
-//                while (nums[i - 1] + j < nums[i]) {
-//                    res.add(nums[i - 1] + j);
-//                    j++;
-//                }
-//            }
-//        if(nums[nums.length-1]!=nums.length){
-//            int j=1;
-//            while (nums[nums.length-1]+j<=nums.length){
-//                res.add(j+nums[nums.length-1]);
-//                j++;
-//            }
-//        }
-//        return res;
-        int left=0;
-        while (left<nums.length){
-            while (left<nums.length&&left+1!=nums[left]){
-                int tmp=nums[left];
-                nums[tmp]=tmp;
-                nums[left]=-1;
-                left++;
-            }
-            left++;
-        }
-        for(int i=0;i<nums.length;i++){
-            if(i+1!=nums[i])
-                res.add(i+1);
-        }
+        if(nums.length<=0)
             return res;
+        Arrays.sort(nums);
+        if(nums[0]!=1){
+            int j=1;
+            while (j<nums[0]){
+                res.add(j);
+                j++;
+            }
+        }
+        for(int i=1;i<nums.length;i++)
+            if(nums[i]!=nums[i-1]&&nums[i]!=nums[i-1]+1) {
+                int j = 1;
+                while (nums[i - 1] + j < nums[i]) {
+                    res.add(nums[i - 1] + j);
+                    j++;
+                }
+            }
+        if(nums[nums.length-1]!=nums.length){
+            int j=1;
+            while (nums[nums.length-1]+j<=nums.length){
+                res.add(j+nums[nums.length-1]);
+                j++;
+            }
+        }
+        return res;
     }
 }
