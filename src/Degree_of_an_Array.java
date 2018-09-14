1ea61ec63e198fa2f8b560f78a5379e410675cef
Defu95 <wangdefu95@gmail.com>
2018-09-14 15:58:42 +0800

    leetcode 697

--- src/Degree_of_an_Array.java
+++ src/Degree_of_an_Array.java
@@ -5,12 +5,12 @@ import java.util.Map;
 
 public class Degree_of_an_Array {
     public static void main(String[] args) {
-        int[] nums = {1, 2, 2, 3, 1, 4, 2};
+//        int[] nums = {1, 2, 2, 3, 1, 4, 2};
+        int[] nums = {1, 2, 2, 3, 1};
         System.out.print(findShortestSubArray(nums));
     }
 
     public static int findShortestSubArray(int[] nums) {
-        int len = nums.length;
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int a : nums) {
             if (!map.containsKey(a))
@@ -22,10 +22,18 @@ public class Degree_of_an_Array {
         }
         Collection<Integer> collect = map.values();
         int maxfre = Collections.max(collect);
-        int num = 0;
+        int num = -1;
         for (Map.Entry<Integer, Integer> ent : map.entrySet())
-            if (ent.getValue() == maxfre)
-                num = ent.getKey();
+            if (ent.getValue() == maxfre) {
+                if (num == -1)
+                    num = findsub(nums, ent.getKey());
+                else
+                    num = Math.min(num, findsub(nums, ent.getKey()));
+            }
+        return num;
+    }
+
+    public static int findsub(int[] nums, int num) {
         int left = 0, right = nums.length - 1;
         while (nums[left] != num)
             left++;
