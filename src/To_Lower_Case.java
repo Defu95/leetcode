7efad3419601717b4ce799bf9a9e25a25358a148
Defu95 <wangdefu95@gmail.com>
2018-09-14 20:57:47 +0800

    leetcode 709

---
+++ src/To_Lower_Case.java
@@ -0,0 +1,10 @@
+public class To_Lower_Case {
+    public static void main(String[] args) {
+        String s="Hello";
+        System.out.print(toLowerCase(s));
+    }
+
+    public static String toLowerCase(String str) {
+        return str.toLowerCase();
+    }
+}
