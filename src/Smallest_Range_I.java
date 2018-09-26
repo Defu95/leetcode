8d9822e77dadc1adaba2dc6933ba205218dfd7d4
Defu95 <wangdefu95@gmail.com>
2018-09-26 11:59:50 +0800

    leetcode 908

--- src/Smallest_Range_I.java
+++ src/Smallest_Range_I.java
@@ -2,20 +2,28 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Smallest_Range_I {
-    public static void main(String[] args){
-        int[] A = {0,10};
+    public static void main(String[] args) {
+        int[] A = {0, 10};
         int K = 2;
-        System.out.println(smallestRangeI(A,K));
+        System.out.println(smallestRangeI(A, K));
     }
+
     public static int smallestRangeI(int[] A, int K) {
-        if(A.length==0)
+        if (A.length == 0)
             return 0;
-        Arrays.sort(A);
-        int len=A.length;
-        if(A[0]+K>=A[len-1]-K)
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int a : A) {
+            if (a > max)
+                max = a;
+            if (a < min)
+                min = a;
+        }
+        int len = A.length;
+        if (min + K >= max - K)
             return 0;
-        else{
-            return (A[len-1]-2*K-A[0]);
+        else {
+            return (max - 2 * K - min);
         }
     }
 }
