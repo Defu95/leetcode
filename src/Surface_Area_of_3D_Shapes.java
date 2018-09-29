89109f9cb58edc2defe19007f8a3f7b424a8367a
Defu95 <wangdefu95@gmail.com>
2018-09-29 23:46:06 +0800

    leetcode 892

--- src/Surface_Area_of_3D_Shapes.java
+++ src/Surface_Area_of_3D_Shapes.java
@@ -9,7 +9,7 @@ public class Surface_Area_of_3D_Shapes {
         int area = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
-                area += grid[i][j] * 4 + 2;
+                if(grid[i][j]>0) area += grid[i][j] * 4 + 2;
                 if (i > 0) area -= Math.min(grid[i - 1][j], grid[i][j]);
                 if (i < grid.length - 1) area -= Math.min(grid[i][j], grid[i + 1][j]);
                 if (j > 0) area -= Math.min(grid[i][j - 1], grid[i][j]);
