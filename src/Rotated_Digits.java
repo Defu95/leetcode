32dafe98cb9c41359f5b3c5b940facd8151f1e30
Defu95 <wangdefu95@gmail.com>
2018-10-02 14:45:26 +0800

    leetcode 788

--- src/Rotated_Digits.java
+++ src/Rotated_Digits.java
@@ -5,33 +5,35 @@ public class Rotated_Digits {
     public static void main(String[] args) {
         System.out.print(rotatedDigits(10));
     }
+
     public static int rotatedDigits(int N) {
         List<Integer> res = new ArrayList<>();
-        for(int i=1;i<=N;i++){
-            String s=String.valueOf(i);
-            int flag=-1;
-            String tmp="";
-            for(int j=0;j<s.length();j++){
-                int k=rotate(s.charAt(j));
-                if(k==10){
-                    flag=1;
+        for (int i = 1; i <= N; i++) {
+            String s = String.valueOf(i);
+            int flag = -1;
+            String tmp = "";
+            for (int j = 0; j < s.length(); j++) {
+                int k = rotate(s.charAt(j));
+                if (k == 10) {
+                    flag = 1;
                     break;
-                }else
-                    tmp+=String.valueOf(k);
+                } else
+                    tmp += String.valueOf(k);
             }
-            if(flag==-1&&Integer.parseInt(tmp)!=i)
+            if (flag == -1 && Integer.parseInt(tmp) != i)
                 res.add(Integer.parseInt(s));
         }
         return res.size();
     }
-    public static int rotate(char ch){
-        int n=ch-'0';
-        if(n==0||n==1||n==8)
+
+    public static int rotate(char ch) {
+        int n = ch - '0';
+        if (n == 0 || n == 1 || n == 8)
             return n;
-        else if(n==2||n==5)
-            return 10/n;
-        else if(n==6||n==9)
-            return 54/n;
+        else if (n == 2 || n == 5)
+            return 10 / n;
+        else if (n == 6 || n == 9)
+            return 54 / n;
         else return 10;
     }
 }
