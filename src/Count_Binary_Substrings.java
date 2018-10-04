97d874d28478d28b8d0efdd7b44047902ede2272
Defu95 <wangdefu95@gmail.com>
2018-10-04 19:09:17 +0800

    leetcode 696

--- src/Count_Binary_Substrings.java
+++ src/Count_Binary_Substrings.java
@@ -7,21 +7,15 @@ public class Count_Binary_Substrings {
     }
 
     public static int countBinarySubstrings(String s) {
-        int p = 1, count = -1, res = 0, flag = 0;
+        int p = 1, count = 0, res = 0, flag = 0;
         if (s.length() == 1)
             return 0;
         for (int i = 1; i < s.length(); i++) {
             if (s.charAt(i) != s.charAt(i - 1)) {
-                if (flag != 0) {
-                    res += Math.min(p, count);
-                    count = i - flag;
-                    flag = i;
-                    p = 1;
-                } else {
-                    count = i - flag;
-                    flag = i;
-                    p = 1;
-                }
+                res += Math.min(p, count);
+                count = i - flag;
+                flag = i;
+                p = 1;
             } else {
                 p++;
             }
