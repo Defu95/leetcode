4bfcd74114ace86525308ce56ac7f27a8a5d6b41
Defu95 <wangdefu95@gmail.com>
2018-10-12 21:48:24 +0800

    leetcode 704

---
+++ src/Binary_Search.java
@@ -0,0 +1,15 @@
+public class Binary_Search {
+    public static void main(String[] args){
+
+    }
+    public static int search(int[] nums, int target) {
+        if(target<nums[0]||target>nums[nums.length-1]||nums.length<=1)
+            return -1;
+        int left=0,right=nums.length;
+        while (nums[left]<target){
+
+        }
+        int mid=left+(right-left)/2;
+    }
+
+}
