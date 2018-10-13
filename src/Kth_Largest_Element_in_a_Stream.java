1e36629da409be9cea6f33b26a052de69e6fc79e
Defu95 <wangdefu95@gmail.com>
2018-10-14 01:12:20 +0800

    leetcode 914

---
+++ src/Kth_Largest_Element_in_a_Stream.java
@@ -0,0 +1,13 @@
+public class Kth_Largest_Element_in_a_Stream {
+    public static void main(String[] args) {
+        int[] arr = {3, 4, 5, 2};
+
+    }
+    public static int KthLargest(int k,int[]nums){
+        return -1;
+    }
+
+    public int add(int val) {
+        return 1;
+    }
+}
